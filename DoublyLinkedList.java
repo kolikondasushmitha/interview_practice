e5f152bd2d63c8520835e5a9354ebcba06cd3aa5
kolikondasushmitha <sushmithaanju12@gmail.com>
2024-08-29 01:01:02 +1000

    DoublyLinkedList

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -2,13 +2,33 @@
 class Node{
     int data;
     Node previous;
-    Node next;
+    Node next;   //instance variables
 
 }
 
 public class DoublyLinkedList {
     Node head;
     Node tail;
+    int length;
+
+
+    public void addFirst(int data){
+        Node newnode = new Node();
+        newnode.data=data;
+
+        if (head==null){
+            head=newnode;
+            tail=newnode;
+        }else{
+            newnode.next=head;
+            head.previous=newnode;
+            head=newnode;
+        }
+
+
+    }
+
+
 
 
 }
